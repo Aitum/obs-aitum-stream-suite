e539fc8d261af1b1611199d7c0b330719633eb84
Exeldro <exelsor@hotmail.com>
2025-10-08 18:45:47 +0200

    Fix adding existing scene

--- src/docks/canvas-dock.cpp
+++ src/docks/canvas-dock.cpp
@@ -2111,22 +2111,27 @@ static void check_descendant(obs_source_t *parent, obs_source_t *child, void *pa
 bool CanvasDock::add_sources_of_type_to_menu(void *param, obs_source_t *source)
 {
 	QMenu *menu = static_cast<QMenu *>(param);
-	CanvasDock *cd = static_cast<CanvasDock *>(menu->parent());
-	auto a = menu->menuAction();
+	auto parent = qobject_cast<QMenu *>(menu->parent());
+	auto a = parent && !parent->menuAction()->data().isNull() ? parent->menuAction() : menu->menuAction();
+	while (parent && qobject_cast<QMenu *>(parent->parent()))
+		parent = qobject_cast<QMenu *>(parent->parent());
+	CanvasDock *cd = static_cast<CanvasDock *>(parent ? parent->parent() : menu->parent());
 	auto t = a->data().toString();
 	auto idUtf8 = t.toUtf8();
 	const char *id = idUtf8.constData();
 	if (strcmp(obs_source_get_unversioned_id(source), id) == 0) {
 		auto name = QString::fromUtf8(obs_source_get_name(source));
 		QList<QAction *> actions = menu->actions();
-		QAction *before = nullptr;
+		QAction *after = nullptr;
 		for (QAction *menuAction : actions) {
-			if (menuAction->text().compare(name) >= 0)
-				before = menuAction;
+			if (menuAction->text().compare(name, Qt::CaseInsensitive) >= 0) {
+				after = menuAction;
+				break;
+			}
 		}
 		auto na = new QAction(name, menu);
-		connect(na, &QAction::triggered, [cd, source] { cd->AddSourceToScene(source); });
-		menu->insertAction(before, na);
+		connect(na, &QAction::triggered, cd, [cd, source] { cd->AddSourceToScene(source); }, Qt::QueuedConnection);
+		menu->insertAction(after, na);
 		struct descendant_info info = {false, cd->source, obs_scene_get_source(cd->scene)};
 		obs_source_enum_full_tree(source, check_descendant, &info);
 		na->setEnabled(!info.exists);
