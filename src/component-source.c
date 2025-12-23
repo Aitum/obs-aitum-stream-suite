bdf75aa2f3020da137b8a44f6945ef2d03ea7fb8
Exeldro <exelsor@hotmail.com>
2025-12-23 17:45:26 +0100

    Fix texture use for component rendering

--- src/component-source.c
+++ src/component-source.c
@@ -4,6 +4,7 @@
 struct component {
 	obs_source_t *source;
 	obs_weak_source_t *scene_source;
+	gs_texrender_t *scene_render;
 };
 
 static const char *component_getname(void *unused)
@@ -29,6 +30,11 @@ static void component_destroy(void *data)
 		obs_source_release(source);
 	}
 	obs_weak_source_release(component->scene_source);
+	if (component->scene_render) {
+		obs_enter_graphics();
+		gs_texrender_destroy(component->scene_render);
+		obs_leave_graphics();
+	}
 	bfree(component);
 }
 
@@ -101,18 +107,86 @@ static void component_video_tick(void *data, float seconds)
 			obs_canvas_release(canvas);
 		}
 	}
+
+	gs_texrender_reset(component->scene_render);
 }
 
 static void component_video_render(void *data, gs_effect_t *effect)
 {
-	UNUSED_PARAMETER(effect);
 	struct component *component = data;
 
 	obs_source_t *source = obs_weak_source_get_source(component->scene_source);
 	if (!source)
 		return;
-	obs_source_video_render(source);
+
+	const enum gs_color_space current_space = gs_get_color_space();
+	const enum gs_color_space source_space = obs_source_get_color_space(source, 1, &current_space);
+	const enum gs_color_format format = gs_get_format_from_space(source_space);
+
+	if (component->scene_render && (gs_texrender_get_format(component->scene_render) != format)) {
+		gs_texrender_destroy(component->scene_render);
+		component->scene_render = NULL;
+	}
+
+	if (!component->scene_render) {
+		component->scene_render = gs_texrender_create(format, GS_ZS_NONE);
+	}
+	if (!component->scene_render) {
+		obs_source_release(source);
+		return;
+	}
+	uint32_t width = obs_source_get_width(source);
+	uint32_t height = obs_source_get_height(source);
+
+	if (!width || !height) {
+		obs_source_release(source);
+		return;
+	}
+	gs_blend_state_push();
+	gs_reset_blend_state();
+
+	if (gs_texrender_begin_with_color_space(component->scene_render, width, height, source_space)) {
+		struct vec4 clear_color;
+
+		vec4_zero(&clear_color);
+		gs_clear(GS_CLEAR_COLOR, &clear_color, 0.0f, 0);
+		gs_ortho(0.0f, (float)width, 0.0f, (float)height, -100.0f, 100.0f);
+		obs_source_video_render(source);
+		gs_texrender_end(component->scene_render);
+	}
 	obs_source_release(source);
+
+	gs_texture_t *tex = gs_texrender_get_texture(component->scene_render);
+	if (!tex) {
+		gs_blend_state_pop();
+		return;
+	}
+	uint32_t cx = gs_texture_get_width(tex);
+	uint32_t cy = gs_texture_get_height(tex);
+
+	effect = obs_get_base_effect(OBS_EFFECT_DEFAULT);
+
+	gs_blend_state_push();
+	gs_blend_function_separate(GS_BLEND_SRCALPHA, GS_BLEND_INVSRCALPHA, GS_BLEND_ONE, GS_BLEND_INVSRCALPHA);
+
+	const bool linear_srgb = gs_set_linear_srgb(true);
+	const bool previous = gs_framebuffer_srgb_enabled();
+	gs_enable_framebuffer_srgb(true);
+
+	gs_effect_set_texture_srgb(gs_effect_get_param_by_name(effect, "image"), tex);
+
+	gs_technique_t *tech = gs_effect_get_technique(effect, "DrawAlphaDivide");
+	size_t passes = gs_technique_begin(tech);
+	for (size_t i = 0; i < passes; i++) {
+		gs_technique_begin_pass(tech, i);
+		gs_draw_sprite(tex, 0, cx, cy);
+		gs_technique_end_pass(tech);
+	}
+	gs_technique_end(tech);
+	gs_enable_framebuffer_srgb(previous);
+	gs_set_linear_srgb(linear_srgb);
+	gs_blend_state_pop();
+	gs_blend_state_pop();
 }
 
 static inline void mix_audio(float *p_out, float *p_in, size_t pos, size_t count)
@@ -220,7 +294,7 @@ static obs_properties_t *component_get_properties(void *data, void *type_data)
 	UNUSED_PARAMETER(data);
 	UNUSED_PARAMETER(type_data);
 	obs_properties_t *props = obs_properties_create();
-	obs_property_t* p = obs_properties_add_int(props, "cx", obs_module_text("Width"), 1, 16384, 1);
+	obs_property_t *p = obs_properties_add_int(props, "cx", obs_module_text("Width"), 1, 16384, 1);
 	obs_property_int_set_suffix(p, "px");
 	p = obs_properties_add_int(props, "cy", obs_module_text("Height"), 1, 16384, 1);
 	obs_property_int_set_suffix(p, "px");
