6595ee50d2cd1afc6c5c2535b51b4080659eab2d
Exeldro <exelsor@hotmail.com>
2026-02-10 20:47:11 +0100

    do vendor_request_get_docks on the UI thread

--- src/utils/obs-websocket.cpp
+++ src/utils/obs-websocket.cpp
@@ -446,16 +446,23 @@ void vendor_request_get_docks(obs_data_t *request_data, obs_data_t *response_dat
 {
 	UNUSED_PARAMETER(request_data);
 	auto da = obs_data_array_create();
-	auto main_window = static_cast<QMainWindow *>(obs_frontend_get_main_window());
-	auto docks = main_window->findChildren<QDockWidget *>();
-	for (auto &dock : docks) {
-		auto di = obs_data_create();
-		obs_data_set_string(di, "name", dock->objectName().toUtf8().constData());
-		obs_data_set_bool(di, "visible", dock->isVisible());
-		obs_data_set_bool(di, "floating", dock->isFloating());
-		obs_data_array_push_back(da, di);
-		obs_data_release(di);
-	}
+
+	obs_queue_task(
+		OBS_TASK_UI,
+		[](void *param) {
+			auto da = static_cast<obs_data_array_t *>(param);
+			auto main_window = static_cast<QMainWindow *>(obs_frontend_get_main_window());
+			auto docks = main_window->findChildren<QDockWidget *>();
+			for (auto &dock : docks) {
+				auto di = obs_data_create();
+				obs_data_set_string(di, "name", dock->objectName().toUtf8().constData());
+				obs_data_set_bool(di, "visible", dock->isVisible());
+				obs_data_set_bool(di, "floating", dock->isFloating());
+				obs_data_array_push_back(da, di);
+				obs_data_release(di);
+			}
+		},
+		da, true);
 	obs_data_set_array(response_data, "docks", da);
 	obs_data_array_release(da);
 }
